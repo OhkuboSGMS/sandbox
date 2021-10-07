efe2e40c13f99e72a34b6909b7c227570165df43
OhkuboSGMS <sinikake.gokiburi@gmail.com>
2021-10-07 23:39:20 +0900

    シェルの引数を修正

--- silly_phrase_bot/shell/upload.sh
+++ silly_phrase_bot/shell/upload.sh
@@ -1,3 +1,3 @@
-python upload/google_drive_csv.py 1ZapCTVCSiqmWmO8BFf0goVz2s5llVji-VhU-HwavWrY phrase.csv
+python upload/google_drive_csv.py
 bin/silly_phrase_bot.exe --phrase phrase.csv --default default.yaml
 python upload/upload_phrase_json.py
--- silly_phrase_bot/shell/upload_bin.sh
+++ silly_phrase_bot/shell/upload_bin.sh
@@ -1,3 +1,3 @@
-dist/google_drive_csv 1ZapCTVCSiqmWmO8BFf0goVz2s5llVji-VhU-HwavWrY phrase.csv
+dist/google_drive_csv
 bin/silly_phrase_bot.exe --phrase phrase.csv --default default.yaml
 dist/upload_phrase_json
