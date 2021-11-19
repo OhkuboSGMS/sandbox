8c6499d60c6fb1bc86c5463de805bc274163a3b8
OhkuboSGMS <sinikake.gokiburi@gmail.com>
2021-11-19 13:10:15 +0900

    [ymlc] update_config:インスタンスから更新も可能に

--- yaml_config/tests/test_main.py
+++ yaml_config/tests/test_main.py
@@ -22,6 +22,17 @@ def test_update_config():
     CONFIG.Hi.B = "250ml"
     assert CONFIG.Hi.C == "500ml"
     assert CONFIG.NUM_CLASSES == 30
+    # ファイルベースでのアップデート
     CONFIG = update_config(CONFIG, config_yaml_file)
     assert CONFIG.Hi.C == "250ml"
     assert CONFIG.NUM_CLASSES == 12
+    # インスタンスベースでのアップデータ　
+    new_config = Dict()
+    new_config.Hi.C = '300ml'
+    new_config.NUM_CLASSES = 6
+    print(CONFIG)
+    CONFIG = update_config(CONFIG, new_config)
+
+    assert CONFIG.Hi.C == '300ml'
+    assert CONFIG.NUM_CLASSES == 6
+    print(CONFIG)
--- yaml_config/ymlc/main.py
+++ yaml_config/ymlc/main.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import Union
 
 import dictdiffer
 import yaml
@@ -14,21 +15,25 @@ def all_keys(a):
     return keys
 
 
-def update_config(config: Dict, config_file_path: str):
+def update_config(config: Dict, config_file_path: Union[str, Path, Dict]):
     after = config.deepcopy()
-    if not os.path.exists(config_file_path):
-        raise FileNotFoundError(config_file_path)
+    if isinstance(config_file_path, str) or isinstance(config, Path):
+        if not os.path.exists(config_file_path):
+            raise FileNotFoundError(config_file_path)
 
-    with open(config_file_path, "r") as f:
-        yaml_config = yaml.load(f, Loader=yaml.SafeLoader)  # yamlのフォーマットが間違っている場合エラーを表示
-        yaml_keys = all_keys(yaml_config)
-        config_keys = all_keys(config)
-        is_invalid_args = not (set(yaml_keys) <= set(config_keys))
-        # 不要な変数がある場合はエラーを表示, yaml config内でCONFIGが対応していない変数を表示
-        invalid_args = set(yaml_keys) - (set(yaml_keys) & set(config_keys))
-        if is_invalid_args:
-            raise ValueError(f"Invalid args {invalid_args} in {yaml_keys}.")
-        after.update(yaml_config)
+        with open(config_file_path, "r") as f:
+            yaml_config = yaml.load(f, Loader=yaml.SafeLoader)  # yamlのフォーマットが間違っている場合エラーを表示
+    else:
+        yaml_config = config_file_path
+
+    yaml_keys = all_keys(yaml_config)
+    config_keys = all_keys(config)
+    is_invalid_args = not (set(yaml_keys) <= set(config_keys))
+    # 不要な変数がある場合はエラーを表示, yaml config内でCONFIGが対応していない変数を表示
+    invalid_args = set(yaml_keys) - (set(yaml_keys) & set(config_keys))
+    if is_invalid_args:
+        raise ValueError(f"Invalid args {invalid_args} in {yaml_keys}.")
+    after.update(yaml_config)
     return after
 
 
